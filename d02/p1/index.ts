2b2726b3cc38bfde0aba410a2a2c3f2b7a554b6f
Pat Smuk <pat.smuk@gmail.com>
2023-12-02 00:31:41 -0500

    Day 2

--- d02/p1/index.ts
+++ d02/p1/index.ts
@@ -1,8 +1,36 @@
 import { readInputFileLines } from '../../util'
 
-function parseLine(line: string) {
-  return line
+const MAX_GEMS: { [color: string]: number } = {
+  red: 12,
+  green: 13,
+  blue: 14,
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+function parseLine(line: string) {
+  const [game, revealsRaw] = line.split(':')
+  const [_, gameNum] = game.split(' ')
+  const reveals = revealsRaw
+    .split(';')
+    .map((rev) => rev.split(',').map((pair) => pair.substring(1).split(' ')))
+  return { gameNum: parseInt(gameNum), reveals }
+}
+
+const games = readInputFileLines(__dirname, parseLine)
+
+let sum = 0
+
+nextGame: for (const { gameNum, reveals } of games) {
+  for (const reveal of reveals) {
+    for (const [countString, color] of reveal) {
+      const count = parseInt(countString)
+
+      if (count > MAX_GEMS[color]) {
+        continue nextGame
+      }
+    }
+  }
+
+  sum += gameNum
+}
+
+console.log(sum)
--- d02/p2/index.ts
+++ d02/p2/index.ts
@@ -1,8 +1,32 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  const [_, reveals] = line.split(':')
+  const hands = reveals
+    .split(';')
+    .map((hand) => hand.split(',').map((x) => x.substring(1).split(' ')))
+  return { hands }
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+const games = readInputFileLines(__dirname, parseLine)
+
+let sum = 0
+
+for (const { hands } of games) {
+  const maxs: { [color: string]: number } = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  }
+
+  for (const hand of hands) {
+    for (const [countString, color] of hand) {
+      const count = parseInt(countString)
+      maxs[color] = Math.max(maxs[color], count)
+    }
+  }
+
+  sum += maxs.red * maxs.green * maxs.blue
+}
+
+console.log(sum)
