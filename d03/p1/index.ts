192c43547d6a243ff890e3669adf02655b01a7c5
Pat Smuk <pat.smuk@gmail.com>
2023-12-03 00:47:52 -0500

    Day 3

--- d03/p1/index.ts
+++ d03/p1/index.ts
@@ -1,8 +1,57 @@
 import { readInputFileLines } from '../../util'
 
+const PART_NUMBER_PATTERN = /\d+/g
+const SYMBOL_PATTERN = /[^0-9.]/g
+
 function parseLine(line: string) {
-  return line
+  const symbolCols = []
+  for (let i = 0; i < line.length; i++) {
+    if (SYMBOL_PATTERN.test(line[i])) {
+      symbolCols.push(i)
+    }
+  }
+
+  const partNums = [...line.matchAll(PART_NUMBER_PATTERN)].map((match) => ({
+    partNumber: match[0],
+    startIndex: match.index!,
+  }))
+
+  return { partNums, symbolCols }
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+
+// Create a set with the coordinates of all symbols
+const symbolCoords = new Set<string>()
+for (let row = 0; row < inputs.length; row++) {
+  for (const col of inputs[row].symbolCols) {
+    symbolCoords.add(`${row},${col}`)
+  }
+}
+
+let sum = 0
+
+// For each part number of each row...
+for (let row = 0; row < inputs.length; row++) {
+  nextPartNumber: for (const { partNumber, startIndex } of inputs[row]
+    .partNums) {
+    // Go through all the columns the part number occupies (plus the column before and after)
+    for (
+      let col = startIndex - 1;
+      col < startIndex + partNumber.length + 1;
+      col++
+    ) {
+      // Check for a symbol above, below, and on this row
+      if (
+        symbolCoords.has(`${row - 1},${col}`) ||
+        symbolCoords.has(`${row + 0},${col}`) ||
+        symbolCoords.has(`${row + 1},${col}`)
+      ) {
+        sum += parseInt(partNumber)
+        continue nextPartNumber
+      }
+    }
+  }
+}
+
+console.log(sum)
--- d03/p2/index.ts
+++ d03/p2/index.ts
@@ -1,8 +1,68 @@
 import { readInputFileLines } from '../../util'
 
+const PART_NUMBER_PATTERN = /\d+/g
+const SEARCH_DIRECTIONS: [number, number][] = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 0],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+]
+
 function parseLine(line: string) {
-  return line
+  const gearCols = []
+  for (let i = 0; i < line.length; i++) {
+    if (line[i] === '*') {
+      gearCols.push(i)
+    }
+  }
+
+  const partNums = [...line.matchAll(PART_NUMBER_PATTERN)].map((match) => ({
+    partNumber: match[0],
+    startIndex: match.index!,
+  }))
+
+  return { partNums, gearCols }
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+
+// Create a map from coordinates to the part number at that location (if any)
+const partNumLocations = new Map<string, number>()
+for (let row = 0; row < inputs.length; row++) {
+  for (const { partNumber, startIndex } of inputs[row].partNums) {
+    for (let col = startIndex; col < startIndex + partNumber.length; col++) {
+      partNumLocations.set(`${row},${col}`, parseInt(partNumber))
+    }
+  }
+}
+
+let sum = 0
+
+// For each gear of each row...
+for (let row = 0; row < inputs.length; row++) {
+  for (const gearCol of inputs[row].gearCols) {
+    const nearbyPartNum = new Set<number>()
+
+    // Search around the gear in every direction for any part numbers
+    for (const [dx, dy] of SEARCH_DIRECTIONS) {
+      const maybePartNum = partNumLocations.get(`${row + dx},${gearCol + dy}`)
+
+      if (maybePartNum !== undefined) {
+        nearbyPartNum.add(maybePartNum)
+      }
+    }
+
+    // If there are exactly two unique part numbers nearby, they are connected gears
+    if (nearbyPartNum.size === 2) {
+      const partPair = [...nearbyPartNum]
+      sum += partPair[0] * partPair[1]
+    }
+  }
+}
+
+console.log(sum)
