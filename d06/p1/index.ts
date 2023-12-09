888377583714b14ecf3d02c6ce5a309c8e33c9bf
Pat Smuk <pat.smuk@gmail.com>
2023-12-09 15:27:57 -0500

    Day 6

--- d06/p1/index.ts
+++ d06/p1/index.ts
@@ -2,7 +2,25 @@ import { each, range, readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
   return line
+    .split(' ')
+    .slice(1)
+    .filter((x) => x !== '')
+    .map((x) => parseInt(x))
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+const [raceTimes, records] = readInputFileLines(__dirname, parseLine)
+
+const waysToWin = []
+for (const [raceTime, i] of each(raceTimes)) {
+  const record = records[i]
+
+  for (const chargeTime of range(1, raceTime)) {
+    const distance = chargeTime * (raceTime - chargeTime)
+    if (distance > record) {
+      waysToWin.push(raceTime - chargeTime * 2 + 1)
+      break
+    }
+  }
+}
+
+console.log(waysToWin.reduce((prev, curr) => prev * curr, 1))
--- d06/p2/index.ts
+++ d06/p2/index.ts
@@ -1,8 +1,22 @@
-import { each, range, readInputFileLines } from '../../util'
+import { range, readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return parseInt(
+    line
+      .split(' ')
+      .slice(1)
+      .filter((x) => x !== '')
+      .join('')
+  )
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+const [raceTime, record] = readInputFileLines(__dirname, parseLine)
+console.log(raceTime, record)
+
+for (const chargeTime of range(1, raceTime)) {
+  const distance = chargeTime * (raceTime - chargeTime)
+  if (distance > record) {
+    console.log(raceTime - chargeTime * 2 + 1)
+    break
+  }
+}
