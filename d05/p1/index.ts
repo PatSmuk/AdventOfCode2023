06982f797e7e24cbe48f41f6b4a8e96cefc32434
Pat Smuk <pat.smuk@gmail.com>
2023-12-09 22:08:18 -0500

    Day 5

--- d05/p1/index.ts
+++ d05/p1/index.ts
@@ -1,8 +1,86 @@
 import { each, range, readInputFileLines } from '../../util'
 
-function parseLine(line: string) {
-  return line
+function parseLine(line: string, i: number) {
+  if (i === 0) {
+    return line
+      .split(' ')
+      .slice(1)
+      .map((x) => parseInt(x))
+  }
+  if (line.charCodeAt(0) >= 0x30 && line.charCodeAt(0) <= 0x39) {
+    return line.split(' ').map((x) => parseInt(x))
+  }
+  return null
+}
+
+interface MapEntry {
+  destStart: number
+  sourceStart: number
+  length: number
+}
+
+const NEXT_MAP_KEY: { [currentKey: string]: string } = {
+  seedToSoil: 'soilToFertilizer',
+  soilToFertilizer: 'fertilizerToWater',
+  fertilizerToWater: 'waterToLight',
+  waterToLight: 'lightToTemperature',
+  lightToTemperature: 'temperatureToHumidity',
+  temperatureToHumidity: 'humidityToLocation',
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+const seeds = inputs[0]!
+const maps: { [type: string]: MapEntry[] } = {
+  seedToSoil: [],
+  soilToFertilizer: [],
+  fertilizerToWater: [],
+  waterToLight: [],
+  lightToTemperature: [],
+  temperatureToHumidity: [],
+  humidityToLocation: [],
+}
+// Build maps
+{
+  let currentMap = 'seedToSoil'
+  let offset = 3
+  for (;;) {
+    const [destStart, sourceStart, length] = inputs[offset]!
+    maps[currentMap].push({ destStart, sourceStart, length })
+    offset++
+
+    if (inputs[offset] == null) {
+      currentMap = NEXT_MAP_KEY[currentMap]
+      if (!currentMap) {
+        break
+      }
+      while (inputs[offset] == null) {
+        offset++
+      }
+    }
+  }
+}
+
+let closestLocation = Number.POSITIVE_INFINITY
+
+for (const seed of seeds) {
+  let currentMap = 'seedToSoil'
+  let currentId = seed
+
+  for (;;) {
+    for (const { destStart, sourceStart, length } of maps[currentMap]) {
+      // If in bounds of the map, apply the map
+      if (sourceStart <= currentId && currentId < sourceStart + length) {
+        currentId = currentId - sourceStart + destStart
+        break
+      }
+    }
+
+    currentMap = NEXT_MAP_KEY[currentMap]
+    if (!currentMap) {
+      closestLocation = Math.min(closestLocation, currentId)
+      break
+    }
+  }
+}
+
+console.log(closestLocation)
--- d05/p2/index.ts
+++ d05/p2/index.ts
@@ -1,8 +1,144 @@
 import { each, range, readInputFileLines } from '../../util'
 
-function parseLine(line: string) {
-  return line
+function parseLine(line: string, i: number) {
+  if (i === 0) {
+    return line
+      .split(' ')
+      .slice(1)
+      .map((x) => parseInt(x))
+  }
+  if (line.charCodeAt(0) >= 0x30 && line.charCodeAt(0) <= 0x39) {
+    return line.split(' ').map((x) => parseInt(x))
+  }
+  return null
+}
+
+interface MapEntry {
+  destStart: number
+  sourceStart: number
+  length: number
+}
+
+const NEXT_MAP_KEY: { [currentKey: string]: string } = {
+  seedToSoil: 'soilToFertilizer',
+  soilToFertilizer: 'fertilizerToWater',
+  fertilizerToWater: 'waterToLight',
+  waterToLight: 'lightToTemperature',
+  lightToTemperature: 'temperatureToHumidity',
+  temperatureToHumidity: 'humidityToLocation',
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+
+let ranges = [] as [number, number][]
+for (let i = 0; i < inputs[0]!.length; i += 2) {
+  ranges.push([inputs[0]![i], inputs[0]![i + 1]])
+}
+
+const maps: { [type: string]: MapEntry[] } = {
+  seedToSoil: [],
+  soilToFertilizer: [],
+  fertilizerToWater: [],
+  waterToLight: [],
+  lightToTemperature: [],
+  temperatureToHumidity: [],
+  humidityToLocation: [],
+}
+// Build maps
+{
+  let currentMap = 'seedToSoil'
+  let offset = 3
+  for (;;) {
+    const [destStart, sourceStart, length] = inputs[offset]!
+    maps[currentMap].push({ destStart, sourceStart, length })
+    offset++
+
+    if (inputs[offset] == null) {
+      currentMap = NEXT_MAP_KEY[currentMap]
+      if (!currentMap) {
+        break
+      }
+      while (inputs[offset] == null) {
+        offset++
+      }
+    }
+  }
+}
+
+let currentMap = 'seedToSoil'
+
+for (;;) {
+  const mappedRanges = [] as typeof ranges
+
+  // For each map...
+  for (const { destStart, sourceStart, length: mapLength } of maps[
+    currentMap
+  ]) {
+    const applyMap = (original: number) => original - sourceStart + destStart
+    // Keep track of any left-over bits of ranges that get snipped
+    const leftOverRanges = [] as typeof ranges
+
+    // For each range...
+    for (const [rangeStart, rangeLength] of ranges) {
+      // Determine whether the start and/or end is inside the map's span
+      const isStartInMap =
+        rangeStart >= sourceStart && rangeStart < sourceStart + mapLength
+      const isEndInMap =
+        rangeStart + rangeLength - 1 >= sourceStart &&
+        rangeStart + rangeLength - 1 < sourceStart + mapLength
+
+      if (isStartInMap && !isEndInMap) {
+        // range: ----[XXXXXXX]-----
+        //   map: --[XXX]-----------
+        const overlapLength = sourceStart + mapLength - rangeStart
+        mappedRanges.push([applyMap(rangeStart), overlapLength])
+        leftOverRanges.push([
+          sourceStart + mapLength,
+          rangeLength - overlapLength,
+        ])
+      } else if (!isStartInMap && isEndInMap) {
+        // range: ----[XXXXXXX]-----
+        //   map: ----------[XXX]---
+        const overlapLength = rangeStart + rangeLength - sourceStart
+        mappedRanges.push([applyMap(sourceStart), overlapLength])
+        leftOverRanges.push([rangeStart, rangeLength - overlapLength])
+      } else if (isStartInMap && isEndInMap) {
+        // range: ----[XXX]-----
+        //   map: --[XXXXXXX]---
+        mappedRanges.push([applyMap(rangeStart), rangeLength])
+      } else if (
+        sourceStart > rangeStart &&
+        sourceStart < rangeStart + rangeLength
+      ) {
+        // range: ----[XXXXXXX]---
+        //   map: ------[XXX]-----
+        mappedRanges.push([applyMap(sourceStart), mapLength])
+        leftOverRanges.push([rangeStart, sourceStart - rangeStart])
+        leftOverRanges.push([
+          sourceStart + mapLength,
+          rangeStart + rangeLength - (sourceStart + mapLength),
+        ])
+      } else {
+        // range: --------[XXXXXXX]--
+        //   map: --[XXX]------------
+        leftOverRanges.push([rangeStart, rangeLength])
+      }
+    }
+
+    ranges = leftOverRanges
+  }
+
+  // Add mapped ranges back in for next round
+  ranges = [...ranges, ...mappedRanges]
+
+  currentMap = NEXT_MAP_KEY[currentMap]
+  if (!currentMap) {
+    break
+  }
+}
+
+let closestLocation = Number.POSITIVE_INFINITY
+for (const [start] of ranges) {
+  closestLocation = Math.min(start, closestLocation)
+}
+console.log(closestLocation)
