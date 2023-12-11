bf476dd88fad4b4b4bc83ed59491c37031ac641f
Pat Smuk <pat.smuk@gmail.com>
2023-12-11 18:02:46 -0500

    Day 11

--- d11/p1/index.ts
+++ d11/p1/index.ts
@@ -5,4 +5,50 @@ function parseLine(line: string) {
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+
+// Expand rows by doubling any that are empty
+const expandedRows = []
+for (const row of inputs) {
+  expandedRows.push(row)
+  if (row.split('').every((char) => char === '.')) {
+    expandedRows.push(row)
+  }
+}
+
+// Figure out which columns are empty
+const emptyColumns = new Set<number>()
+nextColumn: for (const i of range(0, expandedRows[0].length)) {
+  for (const row of inputs) {
+    if (row[i] !== '.') {
+      continue nextColumn
+    }
+  }
+  emptyColumns.add(i)
+}
+
+// Now for each row, double any elements whose column is in empty columns
+const expandedSpace = []
+for (const row of expandedRows) {
+  expandedSpace.push(
+    row
+      .split('')
+      .flatMap((char, i) => (emptyColumns.has(i) ? ['.', '.'] : char))
+  )
+}
+
+// Sum distances between galaxies
+let distanceSum = 0
+const prevGalaxies: [number, number][] = []
+
+for (const [row, y1] of each(expandedSpace)) {
+  for (const [char, x1] of each(row)) {
+    if (char !== '.') {
+      for (const [x2, y2] of prevGalaxies) {
+        distanceSum += Math.abs(y2 - y1) + Math.abs(x2 - x1)
+      }
+      prevGalaxies.push([x1, y1])
+    }
+  }
+}
+
+console.log(distanceSum)
--- d11/p2/index.ts
+++ d11/p2/index.ts
@@ -1,8 +1,52 @@
 import { each, range, readInputFileLines } from '../../util'
 
+const EXPANSION_FACTOR = 1_000_000
+
 function parseLine(line: string) {
   return line
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+
+// Calculate how far off each Y coordinate is after expansion
+const yOffsets: number[] = []
+let offset = 0
+
+for (const row of inputs) {
+  if (row.split('').every((char) => char === '.')) {
+    offset += EXPANSION_FACTOR - 1 // adjust for the empty row still existing
+  }
+  yOffsets.push(offset)
+}
+
+// Calculate how far off each X coordinate is after expansion
+const xOffsets: number[] = []
+offset = 0
+
+for (const x of range(0, inputs[0].length)) {
+  if (inputs.every((row) => row[x] === '.')) {
+    offset += EXPANSION_FACTOR - 1 // adjust for the empty column still existing
+  }
+  xOffsets.push(offset)
+}
+
+// Sum distances between galaxies, after adjusting coordinates for expansion
+let distanceSum = 0
+const prevGalaxies: [number, number][] = []
+
+for (const [row, y] of each(inputs)) {
+  const y1 = y + yOffsets[y]
+
+  for (const [char, x] of each(row)) {
+    const x1 = x + xOffsets[x]
+
+    if (char !== '.') {
+      for (const [x2, y2] of prevGalaxies) {
+        distanceSum += Math.abs(y2 - y1) + Math.abs(x2 - x1)
+      }
+      prevGalaxies.push([x1, y1])
+    }
+  }
+}
+
+console.log(distanceSum)
