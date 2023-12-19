848175705b0ee0db4a69e32a6b5e97eb019cb089
Pat Smuk <pat.smuk@gmail.com>
2023-12-18 22:10:12 -0500

    Day 18

--- d18/p1/index.ts
+++ d18/p1/index.ts
@@ -1,8 +1,85 @@
 import { each, range, readInputFileLines } from '../../util'
 
+const INSTRUCTION_PATTERN = /(R|L|U|D) (\d+) \(#([0-9a-f]{6})\)/
+
+const DIRECTION_VECTORS = {
+  D: [0, +1] as [number, number],
+  U: [0, -1] as [number, number],
+  R: [+1, 0] as [number, number],
+  L: [-1, 0] as [number, number],
+}
+type Direction = keyof typeof DIRECTION_VECTORS
+
+// "R 6 (#70c710)" -> ['R', 6, '70c710']
 function parseLine(line: string) {
-  return line
+  const [_, direction, steps, colour] = INSTRUCTION_PATTERN.exec(line)!
+  return [direction, parseInt(steps), colour] as [Direction, number, string]
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+const instructions = readInputFileLines(__dirname, parseLine)
+
+const dugCoords = new Set<string>(['0,0'])
+let x = 0
+let y = 0
+let minX = x
+let minY = y
+let maxX = x
+let maxY = y
+
+// Follow the path, keeping track of all the coords we dug and the
+//   bounds of the digging area
+for (const [direction, steps] of instructions) {
+  const [dx, dy] = DIRECTION_VECTORS[direction]
+
+  for (const _ of range(0, steps)) {
+    x += dx
+    y += dy
+
+    minX = Math.min(minX, x)
+    minY = Math.min(minY, y)
+    maxX = Math.max(maxX, x)
+    maxY = Math.max(maxY, y)
+
+    dugCoords.add(`${x},${y}`)
+  }
+}
+
+// Now do a flood fill from just outside the top-left corner, to find out
+//   all the coordinates that are on the outside
+const exteriorCoords = new Set<string>([`${minX - 1},${minY - 1}`])
+const frontier: [number, number][] = [[minX - 1, minY - 1]]
+
+// While there are still squares to visit...
+while (frontier.length > 0) {
+  const [x, y] = frontier.shift()!
+
+  // Check each direction
+  for (const [dx, dy] of Object.values(DIRECTION_VECTORS)) {
+    const x2 = x + dx
+    const y2 = y + dy
+    // Bounds check
+    if (x2 < minX - 1 || x2 > maxX + 1 || y2 < minY - 1 || y2 > maxY + 1) {
+      continue
+    }
+    const key = `${x2},${y2}`
+    // If we've been here before or it was dug out, skip it
+    if (exteriorCoords.has(key) || dugCoords.has(key)) {
+      continue
+    }
+    exteriorCoords.add(key)
+    frontier.push([x2, y2])
+  }
+}
+
+// Now just add up all the squares within bounds that are not external
+let dugSquaresSum = 0
+for (const y of range(minY, maxY + 1)) {
+  let line = ''
+  for (const x of range(minX, maxX + 1)) {
+    const isDug = !exteriorCoords.has(`${x},${y}`)
+    line += isDug ? (dugCoords.has(`${x},${y}`) ? '#' : 'x') : '.'
+    dugSquaresSum += isDug ? 1 : 0
+  }
+  console.log(line)
+}
+console.log('\n' + dugSquaresSum)
--- d18/p2/index.ts
+++ d18/p2/index.ts
@@ -1,8 +1,246 @@
+import assert from 'assert'
 import { each, range, readInputFileLines } from '../../util'
 
-function parseLine(line: string) {
-  return line
+const INSTRUCTION_PATTERN = /(?:R|L|U|D) \d+ \(#([0-9a-f]{5})([0-9a-f]{1})\)/
+
+const DIRECTION_VECTORS = {
+  D: [0, +1] as [number, number],
+  U: [0, -1] as [number, number],
+  R: [+1, 0] as [number, number],
+  L: [-1, 0] as [number, number],
+}
+type Direction = keyof typeof DIRECTION_VECTORS
+const DIRECTIONS: Direction[] = ['R', 'D', 'L', 'U']
+
+type ProjDirection = 'U' | 'D' // Can only project vertically
+// Maps from directions in and out of a Node to the projection direction
+const IN_AND_OUT_TO_PROJECTION: { [pair: string]: ProjDirection } = {
+  'U,L': 'U',
+  'L,D': 'U',
+  'R,U': 'D',
+  'D,R': 'D',
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+function getProjectionDirectionForNode(
+  inDirection: Direction,
+  outDirection: Direction
+): ProjDirection | null {
+  return IN_AND_OUT_TO_PROJECTION[`${inDirection},${outDirection}`] ?? null
+}
+
+interface Node {
+  /** X coordinate of the Node */
+  x: number
+  /** Y coordinate of the Node */
+  y: number
+  /** If this Node is part of a left-hand turn, this holds the direction to project ('U' or 'D') */
+  projectDir: ProjDirection | null
+  /** These are the purple arrows in the diagram */
+  nextNodeDownward: Node | null
+}
+
+// "R 6 (#70c710)" -> ['D', 461937]
+function parseLine(line: string) {
+  const [_, steps, direction] = INSTRUCTION_PATTERN.exec(line)!
+  return [DIRECTIONS[parseInt(direction)], parseInt(steps, 16)] as [
+    Direction,
+    number
+  ]
+}
+
+const instructions = readInputFileLines(__dirname, parseLine)
+assert(
+  instructions[0][0] === 'R' &&
+    instructions[instructions.length - 1][0] === 'U',
+  'expected starting point to be top-left corner'
+)
+
+/*
+ * STEP 1 OF DIAGRAM EXPLANATION
+ */
+
+let x = 0
+let y = 0
+// Build list of nodes in the order they appear in the path, starting with (0,0)
+const nodes: Node[] = [{ x, y, projectDir: null, nextNodeDownward: null }]
+// Also keep track of any horizontal spans for later when we are projecting onto them
+const horizontalSpans: [Node, Node, 'R' | 'L'][] = []
+// Also start figuring out the total area already because there's also a
+//   0.5m wide border around the whole path that we need to include
+// Since the loop below skips the last instruction, add it to border here,
+//   along with 0.25 for the right-hand turn at (0,0)
+let totalArea = 0.25 + instructions[instructions.length - 1][1] / 2
+
+// Ignore last instruction since it just takes us back to the start
+for (const [[direction, steps], i] of each(instructions.slice(0, -1))) {
+  // Move steps-times in this direction
+  const [dx, dy] = DIRECTION_VECTORS[direction]
+  x += dx * steps
+  y += dy * steps
+
+  // Figure out if the new node will need to project up or down
+  const nextDirection = instructions[i + 1][0]
+  const projectDir = getProjectionDirectionForNode(direction, nextDirection)
+
+  // Add area of 0.5m-wide border for the steps we just did
+  totalArea += steps * 0.5
+  // If this is a left-hand turn then remove 0.25m^2 for overlap,
+  //   otherwise add 0.25m^2 for the outside of the corner
+  totalArea += projectDir ? -0.25 : 0.25
+
+  // Create new node, grab previous one, and add new node to list
+  const node: Node = {
+    x,
+    y,
+    projectDir,
+    nextNodeDownward: null,
+  }
+  const previousNode = nodes[nodes.length - 1]
+  nodes.push(node)
+
+  // If we moved downward, then link these two for later
+  // If we moved left or right, add a span for projections later
+  if (direction === 'D') {
+    previousNode.nextNodeDownward = node
+  } else if (direction === 'R') {
+    horizontalSpans.push([previousNode, node, 'R'])
+  } else if (direction === 'L') {
+    horizontalSpans.push([node, previousNode, 'L'])
+  }
+}
+
+/*
+ * STEP 2 OF DIAGRAM EXPLANATION
+ */
+
+// For each node that still needs to project...
+let node = nodes.find((n) => n.projectDir)
+while (node) {
+  const { x, y, projectDir } = node
+
+  let nearestSpan: (typeof horizontalSpans)[0] | null = null
+
+  // Find the nearest horizontal span in the projection direction
+  for (const span of horizontalSpans) {
+    const [left, right] = span
+
+    // If going up make sure span is above us
+    if (projectDir === 'U' && y <= left.y) {
+      continue
+    }
+    // If going down make sure span is below us
+    if (projectDir === 'D' && y >= left.y) {
+      continue
+    }
+    // Make sure we fall somewhere inside span
+    if (left.x > x || right.x < x) {
+      continue
+    }
+
+    if (!nearestSpan) {
+      nearestSpan = span
+    } else {
+      // Update nearestSpan if span is nearer
+      if (projectDir === 'U' && left.y > nearestSpan[0].y) {
+        nearestSpan = span
+      }
+      if (projectDir === 'D' && left.y < nearestSpan[0].y) {
+        nearestSpan = span
+      }
+    }
+  }
+
+  // Should always have a span to project onto now
+  assert(nearestSpan)
+  const [left, right, spanDirection] = nearestSpan
+
+  // Check if we are exactly aligned with the left or right node
+  // If this is the case then we just need to update that node
+  if (left.x === x) {
+    if (projectDir === 'U') {
+      left.nextNodeDownward = node
+    } else {
+      node.nextNodeDownward = left
+    }
+  } else if (right.x === x) {
+    if (projectDir === 'U') {
+      right.nextNodeDownward = node
+    } else {
+      node.nextNodeDownward = right
+    }
+  } else {
+    // Not aligned with either left or right, so need to create
+    // a new node in the middle and split the span into two spans
+
+    // Remove the old span
+    const spanIndex = horizontalSpans.indexOf(nearestSpan)
+    horizontalSpans.splice(spanIndex, 1)
+
+    // Create new node with X aligned with projecting node
+    // and Y aligned with span
+    const newNode: Node = {
+      x,
+      y: left.y,
+      projectDir: null,
+      nextNodeDownward: null,
+    }
+    // Connect new node up to projecting node in right direction
+    if (projectDir === 'U') {
+      newNode.nextNodeDownward = node
+    } else {
+      node.nextNodeDownward = newNode
+    }
+
+    // Insert new node into list of nodes before the one that appears second
+    nodes.splice(
+      nodes.indexOf(spanDirection === 'R' ? right : left),
+      0,
+      newNode
+    )
+
+    // Add the new spans to list of spans
+    horizontalSpans.push([left, newNode, spanDirection])
+    horizontalSpans.push([newNode, right, spanDirection])
+  }
+
+  // Don't need to project this node any more,
+  //   find next one that needs projecting (if any)
+  node.projectDir = null
+  node = nodes.find((n) => n.projectDir)
+}
+
+/*
+ * STEP 3 OF DIAGRAM EXPLANATION
+ */
+
+// Starting with Node A = (0,0)...
+let index = 0
+do {
+  const nodeA = nodes[index]
+
+  // Find Node B by going to the next node, then traversing downward until
+  //   we hit the bottom
+  let nodeB = nodes[index + 1]
+  while (nodeB.nextNodeDownward) {
+    nodeB = nodeB.nextNodeDownward
+  }
+  // Should always be able to go downward
+  assert(nodeB.y !== nodeA.y)
+
+  // Add the area between Node A and Node B
+  totalArea += Math.abs(nodeA.x - nodeB.x) * Math.abs(nodeA.y - nodeB.y)
+  // Safety check since JS can only do 52-bit integers without precision loss
+  assert(totalArea < Number.MAX_SAFE_INTEGER)
+
+  // Traverse the path looking for the next Node A;
+  //   it must have path leading out to the right,
+  //   i.e. next node is same Y coord, and greater X coord
+  do {
+    index = (index + 1) % nodes.length // wrap around to (0,0) again
+  } while (
+    nodes[index].y !== nodes[(index + 1) % nodes.length].y ||
+    nodes[index].x > nodes[(index + 1) % nodes.length].x
+  )
+} while (index !== 0) // Stop when we are back to node (0,0)
+
+console.log(totalArea)
